4d42a5972d7caeba1ce157f8a75ebd0d4f52d439
pumegaming <pumepanarin@hotmail.com>
2020-07-15 06:01:25 +0700

    a

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ client.on('guildMemberRemove', () => {
 });
 
 function updatestat() {
-    client.channels.cache.get('731715856840785951').setName('total member : ' + RTH.fetchMembers.roles.get(memRole).members.size);
+    client.channels.cache.get('731715856840785951').setName('total member : ' + RTH.members.cache.filter(member => !member.memRole).size);
 }
 
 function rollall(message, TEAMmode) {
