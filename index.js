c822c8d5ccd469722dbf2ba8d925932d8140041e
pumegaming <pumepanarin@hotmail.com>
2020-08-29 11:07:18 +0700

    d

--- index.js
+++ index.js
@@ -79,7 +79,10 @@ client.once('ready', () => {
 
 client.on('message', message => { //return;//X
     const LOG = RTH.channels.cache.get('748200435701121035');
-    if (message.channel !== LOG) LOG.send(`${message.author.username} : ${message.channel} - ${message.content}`).catch(console.error);
+    if (message.channel !== LOG) {
+        const content = message.content.replace(/@</gi, '');
+        LOG.send(`${message.author.username} (${message.author.id}) : ${message.channel} - ${content}`).catch(console.error);
+    }
     if (message.type !== 'DEFAULT') return;
     if (message.author.bot) return;
     message.member.roles.add(memRole);
