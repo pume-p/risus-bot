6b3b61acb72dfb9ab28b58f9774ab765d7487a60
pumegaming <pumepanarin@hotmail.com>
2021-04-13 14:47:26 +0700

    a

--- index.js
+++ index.js
@@ -161,12 +161,12 @@ client.on('message', message => { //return;//X
             break;
         case '831413587813466152':
             let allScore;
-            as.messages.fetch("831417840371236894")
+            risusThingScoreBoardCH.messages.fetch("831417840371236894")
                 .then(scoretext => allScore = scoretext.content.split('\n').then(() => {
                     for (let i = 0; i < allScore.length; i++) {
                         if (message.author.id === allScore[i].split('!')[1].split('>')[0]) {
                             {
-                                allScore[i] = `<!@${message.author.id}> ${parseInt(allScore[i].split(' ')[1]) + 1}`;
+                                allScore[i] = `<@!${message.author.id}> ${parseInt(allScore[i].split(' ')[1]) + 1}`;
                                 let ret = '';
                                 allScore.forEach(oneLINER => {
                                     ret += `${oneLINER}\n`;
@@ -176,7 +176,7 @@ client.on('message', message => { //return;//X
                             }
                         }
                     }
-                    scoretext.edit(`${scoretext.content}\n<!@${message.author.id}> 1`);
+                    scoretext.edit(`${scoretext.content}\n<@!${message.author.id}> 1`);
                 }));
             break;
 
