bc58d95bcea2125f1112be17c2247d385d63cfb7
pumegaming <pumepanarin@hotmail.com>
2021-06-02 14:00:08 +0700

    s

--- index.js
+++ index.js
@@ -99,7 +99,9 @@ client.on('message', message => { //return;//X
     }
     if (message.type !== 'DEFAULT') return;
     if (message.author.bot) return;
+    console.log(message.member.nickname);
     if (message.member.nickname.charAt(0) === '<') {
+        console.log(message.member.nickname.charAt(0) === '<');
         message.member.roles.add(memRole);
         message.member.roles.remove(gusRole);
     }
